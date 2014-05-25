0397515575fb96dcbd1b439676522bb6ec6910f0
Adam Malone <adam@adammalone.net>
2014-05-25 23:55:24 +1000

    injected request into the event subscriber

--- lib/Drupal/ip_ranges/EventSubscriber/IPRangesEventSubscriber.php
+++ lib/Drupal/ip_ranges/EventSubscriber/IPRangesEventSubscriber.php
@@ -2,17 +2,21 @@
 
 namespace Drupal\ip_ranges\EventSubscriber;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class IPRangesEventSubscriber implements EventSubscriberInterface {
 
+  public $request;
+
+  public function __construct(Request $request) {
+    $this->request = $request;
+  }
+
   public function onKernelRequest(GetResponseEvent $event) {
-    $request = \Drupal::service('request');
-
     drupal_set_message('event subscriber');
-
   }
 
   static function getSubscribedEvents() {
